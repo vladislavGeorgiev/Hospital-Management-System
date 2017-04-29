0bf4ccab271c0ab9a53ee38d985b3ebf5e3e9fcb
vladislavGeorgiev <dataibaaa@gmail.com>
2017-04-29 09:55:38 +0300

    Final

---
+++ HospitalManagementSystem/Models/Patients/MyPatientsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalManagementSystem.Models.Patients
+{
+    public class MyPatientsModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Condition { get; set; }
+        public int Room { get; set; }
+
+    }
+}
